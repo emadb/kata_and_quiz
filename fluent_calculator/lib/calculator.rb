b632916a09a4bdc5ced98255d6ca361091293a46
Emanuele DelBono <emanuele@codiceplastico.com>
2014-05-19 20:57:36 +0200

    differen implementantio

--- fluent_calculator/lib/calculator.rb
+++ fluent_calculator/lib/calculator.rb
@@ -1,18 +1,35 @@
+# class Calculator
+
+#   @@numbers = {one: 1, two: 2}
+#   @@operators = {plus: :+}
+
+#   def method_missing(name)
+#     if @first_number.nil?
+#       @first_number = @@numbers[name]
+#       return self
+#     end
+#     if @operator.nil?
+#       @operator = @@operators[name]
+#       return self
+#     end
+#     @second_number = @@numbers[name]
+#     @first_number.send @operator, @second_number
+#   end
+# end
+
 class Calculator
+  
+  @@numbers = {one: '1', two: '2', plus: '+'}
 
-  @@numbers = {one: 1, two: 2}
-  @@operators = {plus: :+}
-
+  def initialize
+    @current = ''
+  end
+    
   def method_missing(name)
-    if @first_number.nil?
-      @first_number = @@numbers[name]
-      return self
+    @current << @@numbers[name]
+    if @current.length == 3
+      return eval(@current)
     end
-    if @operator.nil?
-      @operator = @@operators[name]
-      return self
-    end
-    @second_number = @@numbers[name]
-    @first_number.send @operator, @second_number
+    self
   end
 end
