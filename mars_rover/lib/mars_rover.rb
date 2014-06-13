7f67634bcc4bf11537f9c9430772711e6582d341
Emanuele DelBono <emanuele@codiceplastico.com>
2014-06-13 17:46:27 +0200

    more cases

--- mars_rover/lib/mars_rover.rb
+++ mars_rover/lib/mars_rover.rb
@@ -1,2 +1,41 @@
 class MarsRover
+  def initialize(x, y, direction)
+    @position = Position.new(x, y)
+    @direction = direction
+  end
+
+  def position
+    @position
+  end
+
+  def direction
+    @direction
+  end
+
+  def send_commands(commands)
+    commands.each do |c|
+      if @direction == 'N' 
+        @position.decrease_y if c == 'f'
+        @position.increase_y if c == 'b'  
+        @direction = 'W' if c == 'l'
+        @direction = 'E' if c == 'r'
+      end
+    end
+  end
+end
+
+class Position
+  attr_reader :x, :y
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def decrease_y
+    @y = @y - 1
+  end
+
+  def increase_y
+    @y = @y + 1
+  end
 end
--- mars_rover/spec/mars_rover_spec.rb
+++ mars_rover/spec/mars_rover_spec.rb
@@ -2,10 +2,44 @@ require './lib/mars_rover'
 
 describe MarsRover do
   before do
-    @rover = MarsRover.new
+    @rover = MarsRover.new(4,5, 'N')
   end
-  
+
   it 'is not nil' do
     expect(@rover).not_to be_nil
+  end
+
+  it 'create a new rover at given position with give direction' do
+    expect(@rover.position.x).to eq(4)
+    expect(@rover.position.y).to eq(5)
+    expect(@rover.direction).to eq('N')
+  end
+
+  it 'moves forward when receive a f command' do
+    @rover.send_commands(['f'])
+    expect(@rover.position.x).to eq(4)
+    expect(@rover.position.y).to eq(4)
+  end
+
+  it 'moves forward two times when receive two f command' do
+    @rover.send_commands(['f','f'])
+    expect(@rover.position.x).to eq(4)
+    expect(@rover.position.y).to eq(3)
+  end
+
+  it 'moves backwards when receive a b command' do
+    @rover.send_commands(['b'])
+    expect(@rover.position.x).to eq(4)
+    expect(@rover.position.y).to eq(6)
+  end
+
+  it 'turns left when receive a l command' do
+    @rover.send_commands(['l'])
+    expect(@rover.direction).to eq('W')
+  end
+
+  it 'turns right when receive a r command' do
+    @rover.send_commands(['r'])
+    expect(@rover.direction).to eq('E')
   end
 end
