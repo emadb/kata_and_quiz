b43bd977b98e2d8edcb2f1e3121515cf5ac51726
Emanuele DelBono <emanuele@codiceplastico.com>
2014-07-17 19:24:11 +0200

    second story

--- tennis_game/lib/game.rb
+++ tennis_game/lib/game.rb
@@ -1,24 +1,27 @@
 class Game
 
   def initialize
-    @scores = [0, 15, 30, 40]
+    @scores = ['love', 'fifteen', 'thirty', 'forty']
     @players = [0,0]
   end
 
-  def win_player_1
-    add_point_to(0)
+  def score_player_1(s)
+    @players[0] = s
   end
 
-  def win_player_2
-    add_point_to(1)
+  def score_player_2(s)
+    @players[1] = s
   end
 
-  def add_point_to(p)
-    @players[p] = @players[p] + 1
-  end
   def score
     p1 = @scores[@players[0]]
     p2 = @scores[@players[1]]
+    if p1 == p2 && p1 == 'forty'
+      return 'deuce'
+    end
+    if p1 == p2
+      return "#{p1}-all"
+    end
     "#{p1}-#{p2}"
   end
 end
--- tennis_game/spec/game_spec.rb
+++ tennis_game/spec/game_spec.rb
@@ -5,16 +5,50 @@ describe Game do
     @game = Game.new
   end
 
-  it 'When player one wins one point, the score is fifteen-love. ' do
-    @game.win_player_1
-    expect(@game.score).to eq('15-0')
+  context 'Winning Points Increases Score' do
+
+    it 'When player one wins one point, the score is fifteen-love. ' do
+      @game.score_player_1(1)
+      expect(@game.score).to eq('fifteen-love')
+    end
+
+    it 'When player one wins one point and player two wins one point, the score is fifteen-all. ' do
+      @game.score_player_1(1)
+      @game.score_player_2(1)
+      expect(@game.score).to eq('fifteen-all')
+    end
+
+    it 'When player one wins two points and player two wins three points, the score is thirty-forty.' do
+      @game.score_player_1(2)
+      @game.score_player_2(3)
+      expect(@game.score).to eq('thirty-forty')
+    end
+
+    it 'When player one wins no points and player two wins three points, the score is love-forty.' do
+      @game.score_player_2(3)
+      expect(@game.score).to eq('love-forty')
+    end
   end
 
-  it 'When player one wins one point and player two wins one point, the score is fifteen-all. ' do
-    @game.win_player_1
-    @game.win_player_2
-    @game.win_player_2
-    expect(@game.score).to eq('15-30')
+  context 'Deuce and Advantage are Scored Correctly' do
+    it 'When player one wins three points and player two wins three points, the score is deuce' do
+      @game.score_player_1(3)
+      @game.score_player_2(3)
+      expect(@game.score).to eq('deuce')
+    end
+
+    # it 'When player one wins four points and player two wins three points, the score is advantage player one. ' do
+    #   @game.win_player_1
+    #   @game.win_player_1
+    #   @game.win_player_1
+    #   @game.win_player_1
+    #   @game.win_player_2
+    #   @game.win_player_2
+    #   @game.win_player_2
+    #   expect(@game.score).to eq('advantage player one')
+    # end
+
+    
   end
 
 end
