76943c221bbfa6672ed7fcffba037854cb28a12e
Emanuele DelBono <emanuele@codiceplastico.com>
2014-07-17 19:07:50 +0200

    first tests

--- tennis_game/lib/game.rb
+++ tennis_game/lib/game.rb
@@ -1,3 +1,24 @@
 class Game
 
+  def initialize
+    @scores = [0, 15, 30, 40]
+    @players = [0,0]
+  end
+
+  def win_player_1
+    add_point_to(0)
+  end
+
+  def win_player_2
+    add_point_to(1)
+  end
+
+  def add_point_to(p)
+    @players[p] = @players[p] + 1
+  end
+  def score
+    p1 = @scores[@players[0]]
+    p2 = @scores[@players[1]]
+    "#{p1}-#{p2}"
+  end
 end
--- tennis_game/spec/game_spec.rb
+++ tennis_game/spec/game_spec.rb
@@ -5,8 +5,16 @@ describe Game do
     @game = Game.new
   end
 
-  it 'do something' do
-    
+  it 'When player one wins one point, the score is fifteen-love. ' do
+    @game.win_player_1
+    expect(@game.score).to eq('15-0')
+  end
+
+  it 'When player one wins one point and player two wins one point, the score is fifteen-all. ' do
+    @game.win_player_1
+    @game.win_player_2
+    @game.win_player_2
+    expect(@game.score).to eq('15-30')
   end
 
 end
