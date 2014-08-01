285bcdae64c1981a86c80d3cead7414d4fc52bd3
Emanuele DelBono <emanuele@codiceplastico.com>
2014-08-01 16:09:09 +0200

    refactored berlin clock in elixir: more idiomatic

--- berlin_clock_ex/lib/berlin_clock.ex
+++ berlin_clock_ex/lib/berlin_clock.ex
@@ -1,62 +1,52 @@
 import Integer, only: :macros
 
 defmodule BerlinClock do
-	def parse (time) do
+  def parse (time) do
 
-		[h, m, s] = String.split(time, ":") 
-			|> Enum.map fn n -> Integer.parse(n) 
-			|> elem(0) end 
+    [s, m, h] = parse_time(time, [])
 
-		seconds = get_seconds(s)
-		single_minutes = get_single_minutes(m)
-		fives_minutes = get_fives_minutes(m)
-		single_hours = get_single_hours(h)
-		fives_hours = get_fives_hours(h)
+    seconds = get_seconds(s)
+    single_minutes = build_part("Y", rem(m, 5), 4)
+    fives_minutes = build_part(create_fives_minutes(div(m, 5)), 11)
+    single_hours = build_part("R", rem(h, 5), 4)
+    fives_hours = build_part("R", div(h, 5), 4)
 
-		[seconds, fives_hours, single_hours, fives_minutes, single_minutes]
-	end
+    [seconds, fives_hours, single_hours, fives_minutes, single_minutes]
+  end
 
-	def get_fives_hours(h) do
-		number_of_r = div(h, 5)
-		String.ljust(String.duplicate("R", number_of_r), 4, ?O)
-	end
+  def parse_time("", acc) do
+    acc
+  end
 
-	def get_single_hours(h) do
-		number_of_r = rem(h, 5)
-		String.ljust(String.duplicate("R", number_of_r), 4, ?O)
-	end
+  def parse_time(":" <> s, acc) do 
+    parse_time(s, acc)
+  end
 
-	def get_fives_minutes(m) do
-		number_of_y = div(m, 5)
-		String.ljust(create_fives_minutes(number_of_y), 11, ?O)
-	end
+  def parse_time(s, acc) do
+      {num, s} = Integer.parse(s)
+      parse_time(s, [num | acc])
+  end
 
-	def create_fives_minutes(1) do
-		"Y"
-	end
+  def build_part(pattern, repeat \\ 1, field_length) do
+    String.ljust(String.duplicate(pattern, repeat), field_length, ?O)
+  end
 
-	def create_fives_minutes(n) when n < 1 do
-		""
-	end
+  def create_fives_minutes(m) when m >= 0 do
+    create_fives_minutes(m, "")
+  end
+    
+  def create_fives_minutes(0, acc) do 
+    acc
+  end
 
-	def create_fives_minutes(n) when n > 1 do
-		if rem(n,3) == 0 do
-			light = "R"
-		else
-			light = "Y"
-		end
-		[create_fives_minutes(n-1), light ] |> Enum.join
-	end
+  def create_fives_minutes(m, acc) when rem(m,3) == 0 do
+    create_fives_minutes(m-1, "R" <> acc)
+  end
 
-	def get_single_minutes(m) do
-		number_of_y = rem(m, 5)
-		String.ljust(String.duplicate("Y", number_of_y), 4, ?O)
-	end
-
-	def get_seconds(s) do
-		cond do
-			s |> even? -> "Y"
-			s |> odd?  -> "O"
-		end
-	end
+  def create_fives_minutes(m, acc) do
+    create_fives_minutes(m-1, "Y" <> acc)
+  end
+  
+  def get_seconds(s) when even?(s), do: "Y"
+  def get_seconds(_), do: "O"
 end
