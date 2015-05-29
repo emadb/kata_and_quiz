a1cc3fd74fa76468b8c7b475213a31c7d10b5cfe
Emanuele DelBono <emanuele@codiceplastico.com>
2015-05-29 17:30:58 +0200

    v02 with 1 and 2 neighbours

--- game_of_life_ex/lib/game_of_life.ex
+++ game_of_life_ex/lib/game_of_life.ex
@@ -1,25 +1,44 @@
 defmodule Cell do
   def create(my_x \\ 0, my_y \\ 0, parent) do
-    neighbour = 0
-    IO.puts "caricato#{my_x} #{my_y}" 
     send parent, {:position, %{x: my_x , y: my_y}}
-
-    receive do
-      {sender, :where_are_you} ->
-        send sender, {:position, %{x: my_x , y: my_y}}
-      
-      {sender, :tick} ->
-        if(neighbour != 2) do
-          exit(:kill)
-        end
-
-      {sender, :cell_position, %{x: x , y: y}} ->
-        
-        if(abs(x-my_x) < 2 && abs(y-my_y) < 2) do
-          
-          neighbour = neighbour + 1
-        end
-    end
+    listen(my_x, my_y, [])
   end
   
-end 
+  def listen(my_x, my_y, neighbours) do
+    receive do
+      {:alive, position} ->
+        if(abs(position[:x] - my_x) < 2 && abs(position[:y] - my_y) < 2) do  
+          listen(my_x, my_y, Enum.concat(neighbours, [position]))
+        else
+          listen(my_x, my_y, neighbours)
+        end
+
+      {:tick} ->
+        IO.inspect Enum.count(neighbours)
+        if (Enum.count(neighbours) < 2) do
+          Process.exit(self(), :kill)
+        end
+        listen(my_x, my_y,[])
+    end
+  end
+
+end
+
+defmodule World do
+  def create do
+    receive do
+      {sender,:position, position} ->
+        create      
+
+      {sender,:initialize, number_of_cells} ->
+        cells = Enum.map(1..number_of_cells, 
+          fn n -> 
+            spawn(Cell, :create, [n, n, self()])
+            end
+          )
+        send sender, {:initialized, cells}
+        create
+    
+    end
+  end
+end
--- game_of_life_ex/test/game_of_life_test.exs
+++ game_of_life_ex/test/game_of_life_test.exs
@@ -2,84 +2,37 @@ defmodule GameOfLifeTest do
   import :timer
   use ExUnit.Case
 
+  test "create a new cell. It should send its position to the world" do
+    world_pid = spawn(World, :create, [])
+    send world_pid, {self(), :initialize, 10 }
 
-  def listen(pids) do
     receive do
-      {:position, position} ->
-        IO.inspect position
-        broadcast(pids, :cell_position, position)
-        listen(pids)
+      {:initialized,positions} ->
+        IO.inspect positions
     end
-  end
+    
+  end 
 
-  test 'Any live cell with 2 live neighbours live' do
-    pids = []
-    pids = pids ++ [spawn(Cell, :create, [1, 2, self()])]
-    pids = pids ++ [spawn(Cell, :create, [1, 1, self()])]
-    pids = pids ++ [spawn(Cell, :create, [1, 3, self()])]
+  test "A cell with one neighbour should die" do
+    cell = spawn(Cell, :create, [3,4, self()])
+    send cell, {:alive, %{x: 4, y: 4}}
+    send cell, {:tick}
 
-    listen pids
-    IO.puts "test"
     sleep 20
-    
-    broadcast(pids, :tick)
-    [h | tail] = pids
-    [hh | _] = tail
-    assert Process.alive?(h) == true
-    assert Process.alive?(hh) == true
+  
+    isAlive = Process.alive?(cell)
+    assert  isAlive == false
   end
 
-  def broadcast(pids, message, args \\ nil) do
-    Enum.each(pids, 
-      fn p -> 
-       send p, {message, args} 
-      end)
-  end
-
-  # test 'create cell should return a live cell in position 0,0' do
-  #   cell = spawn(Cell, :create, [1, 2, self()])
-  #   receive do
-  #     {:position, position} -> 
-  #       assert position[:x] == 1
-  #       assert position[:y] == 2
-  #   end
-  # end
-
-  # test 'a lonely cell will die' do
-  #   cell = spawn(Cell, :create, [1, 2])
-  #   send cell, {self, :tick}
-  #   sleep 1
-  #   assert Process.alive?(cell) == false
-  # end
-  # TODO: invece di chiedere le coordinate, meglio che ogni cella venga notificata della nascita e della
-  # morte di tutte le altre celle (ogni cella che nasce dice a tutti sono nata). Chi riceve, 
-  # se e' un suo vicino ne tiene traccia, altrimenti ignora
-
-  # test 'create cell with coordinates should return a live cell in a predefined positions' do
-  #   cell = GameOfLife.create_cell(1,2)
-  #   assert cell[:alive] == true
-  #   assert cell[:x] == 1
-  #   assert cell[:y] == 2
-  # end
-
-  # test 'Any live cell with 0 live neighbours dies, as if caused by underpopulation' do
-  #   cell = GameOfLife.create_cell
-  #   [next_cell | _] = GameOfLife.next_era [cell]
-  #   assert next_cell[:alive] == false
-  # end
-
-  # test 'Any live cell with 1 live neighbours dies, as if caused by underpopulation' do
-  #    cell1 = GameOfLife.create_cell(1,1)
-  #    cell2 = GameOfLife.create_cell(1,2)
+  test "A cell with two neighbours should live" do
+    cell = spawn(Cell, :create, [3,4, self()])
+    send cell, {:alive, %{x: 4, y: 4}}
+    send cell, {:alive, %{x: 3, y: 3}}
+    send cell, {:tick}
     
-  #    next_world = GameOfLife.next_era [cell1, cell2]
+    sleep 20
 
-  #    cell1_next = Enum.find(next_world, fn(c) -> c[:x] == cell1[:x] and c[:y] == cell1[:y]  end)
-  #    cell2_next = Enum.find(next_world, fn(c) -> c[:x] == cell2[:x] and c[:y] == cell2[:y]  end)
-     
-  #    assert cell1_next[:alive] == false
-  #    assert cell2_next[:alive] == false
-  #  end
-
-
+    isAlive = Process.alive?(cell)
+    assert  isAlive == true
+  end
 end
