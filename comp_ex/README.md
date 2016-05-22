5ef55b5d0b62105faa9360ab1019c0538ab314f0
Emanuele DelBono <emanuele@codiceplastico.com>
2016-05-22 08:50:47 +0200

    new kata

--- comp_ex/README.md
+++ comp_ex/README.md
@@ -1,4 +1,4 @@
-CirpoEx
+Comp
 =======
 
 ** TODO: Add description **
--- comp_ex/mix.exs
+++ comp_ex/mix.exs
@@ -1,8 +1,8 @@
-defmodule CirpoEx.Mixfile do
+defmodule Comp.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :cirpo_ex,
+    [app: :comp,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
