9cfad6e216b00cebc4f0b498400e1308f30e6fd7
Emanuele DelBono <emanuele@codiceplastico.com>
2016-05-22 09:00:40 +0200

    bowling first test

--- bowling/libs/bowling.js
+++ bowling/libs/bowling.js
@@ -1,46 +1,9 @@
-module.exports = (function() {
+module.exports = {
 
-	var Minesweeper = function(r, c) {
-    this.rows = r;
-    this.cols = c;
-  };
-  function isAStar(row, col, rows){
-    return (rows[row] && rows[row][col] == '*') | 0;
+  score: (input) => {
+    var frames = input.split('')
+    return frames.reduce((acc, f) => {
+      return acc + (parseInt(f) || 0)
+    }, 0)
   }
-
-  function countStarNearby(row, col, rows){
-		var count = 0
-      + isAStar(row, col-1, rows)
-      + isAStar(row, col+1, rows)
-      + isAStar(row-1, col-1, rows)
-      + isAStar(row-1, col, rows)
-      + isAStar(row-1, col+1, rows)
-      + isAStar(row+1, col-1, rows)
-      + isAStar(row+1, col, rows)
-      + isAStar(row+1, col+1, rows);
-
-    return count;
-  }
-
-  Minesweeper.prototype.process  = function(input) {
-    var rows = input.split("\n").map(function(r){ return r.split("") });
-    var result = [];
-
-    rows.forEach(function(row, r){
-      var newRow = row.map(function(e, c){
-        if (e === "*"){
-          return e;
-        } else {
-          return countStarNearby(r, c, rows);
-        }
-      });
-      result.push(newRow.join(""));
-
-    }.bind(this));
-    return result.join("\n");
-  };
-
-
-	return Minesweeper;
-
-})();
+}
--- bowling/test/bowling_test.js
+++ bowling/test/bowling_test.js
@@ -1,50 +1,12 @@
 var sinon = require("sinon");
-var should = require("should");
-var Minesweeper = require("../libs/minesweeper");
+var bowling = require("../libs/bowling");
+var assert = require('chai').assert
 
-describe("Minesweeper", function() {
-  var ms;
-
-  it("process should display this string", function() {
-    ms = new Minesweeper(4,4);
-    var result = ms.process("*...\n....\n.*..\n....");
-    result.should.be.equal("*100\n2210\n1*10\n1110");
-  });
-
-  it("process 1 char * should return *", function() {
-    ms = new Minesweeper(1,1);
-    var result = ms.process("*");
-    result.should.be.equal("*");
-  });
-
-  it("process 2 chars *. should return *1", function() {
-    ms = new Minesweeper(1,2);
-    var result = ms.process("*.");
-    result.should.be.equal("*1");
-  });
-
-  it("process 2 chars .* should return 1*", function() {
-    ms = new Minesweeper(1,2);
-    var result = ms.process(".*");
-    result.should.be.equal("1*");
-  });
-
-  it("process 3 chars .*. should return 1*1", function() {
-    ms = new Minesweeper(1,3);
-    var result = ms.process(".*.");
-    result.should.be.equal("1*1");
-  });
-
-  it("process 4 chars *.*. should return *2*1", function() {
-    ms = new Minesweeper(1,4);
-    var result = ms.process("*.*.");
-    result.should.be.equal("*2*1");
-  });
-
-  it("two rows of 2 chars *. .. should return *1 11", function() {
-    ms = new Minesweeper(2,2);
-    var result = ms.process("*.\n..");
-    result.should.be.equal("*1\n11");
+describe("Bowling", function() {
+  
+  it("9-9-9-9-9-9-9-9-9-9- should return 90", function() {
+   var score = bowling.score('9-9-9-9-9-9-9-9-9-9-')
+   assert.equal(90, score)
   });
 
 
