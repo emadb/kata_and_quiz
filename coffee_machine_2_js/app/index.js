77f91880f13aa9c922ba2953750fc2ebda482491
Emanuele DelBono <emanuele@codiceplastico.com>
2017-02-10 17:11:36 +0100

    some stories

--- coffee_machine_2_js/app/index.js
+++ coffee_machine_2_js/app/index.js
@@ -2,10 +2,28 @@
 
 function CoffeeMachine(){
   this.credit = 0
+  this.coins = []
 }
 
 CoffeeMachine.prototype.switchOn = function() { }
 
+CoffeeMachine.prototype.insertCoin = function(coin){
+  this.coins.push(coin)
+  this.credit += coin
+}
 
+CoffeeMachine.prototype.makeCoffee = function(){
+  if (this.credit >= 30)
+    return {}
+  return {text: '30cents for a coffee'}
+}
+
+CoffeeMachine.prototype.insertKey = function(obj){
+  this.credit = obj.credit
+}
+
+CoffeeMachine.prototype.cancel = function() {
+  return this.coins
+}
 
 module.exports = CoffeeMachine
